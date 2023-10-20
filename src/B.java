f0848774db7e920fae92c35c73f9f655c6a9c82f
Anthonypradosolis <apradosolis@danielcastelao.org>
2023-10-20 10:02:38 +0200

    actu B

--- src/B.java
+++ src/B.java
@@ -2,4 +2,5 @@ public class B {
     public static void main(String[] args) {
         System.out.println("Mateo mamame la pinga");
         System.out.println("JEJE GOD");
+        System.out.println("Sergio grito, noonooooo");
     }
